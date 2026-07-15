4dbdc09768aac229bd13d2a85a20e0d99a298def
hiuphngok <phieu130425@gmail.com>
2026-07-16 01:14:18 +0700

    backend add to cart and get cart

--- backend/src/router/cartRouter.js
+++ backend/src/router/cartRouter.js
@@ -1,18 +1,26 @@
 const express = require('express');
+const { auth } = require('../middleware/auth'); // Nhớ import auth
 const {
   getAllCarts,
   getCartById,
   createCart,
+  addToCart,
+  removeItem,
   updateCart,
   deleteCart,
+  getMyCart
 } = require('../controller/cartController');
 
 const CartRouter = express.Router();
 
+CartRouter.get('/my-cart', auth, getMyCart); // Đặt lên trước /:id
+CartRouter.post('/add', auth, addToCart);
+CartRouter.delete('/remove/:productId', auth, removeItem);
+
 CartRouter.get('/', getAllCarts);
 CartRouter.get('/:id', getCartById);
 CartRouter.post('/', createCart);
-CartRouter.put('/:id', updateCart);
-CartRouter.delete('/:id', deleteCart);
+CartRouter.put('/:id', auth, updateCart);
+CartRouter.delete('/:id', auth, deleteCart);
 
 module.exports = CartRouter;
